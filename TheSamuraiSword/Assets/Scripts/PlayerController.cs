1f58b1eb79713f7bcd2e0142dfab188c0593ba17
emiruyan <emir-uyn@hotmail.com>
2023-03-01 00:24:54 +0300

    save

--- TheSamuraiSword/Assets/Scripts/PlayerController.cs
+++ TheSamuraiSword/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.UI;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
@@ -10,10 +11,14 @@ public class PlayerController : MonoBehaviour
 {
   [SerializeField] private float speed;
   [SerializeField] private DynamicJoystick joystick;
+  [SerializeField] private Image joystickBg;
+  public GameObject sword;
+  public Transform rightHand;
   [SerializeField] private Rigidbody rb;
-   private Animator playerAnimator;
-
-  private void Awake()
+  private bool isRunning;
+  private Animator playerAnimator;
+  
+   private void Awake()
   {
       playerAnimator = GetComponent<Animator>();
   }
@@ -21,6 +26,7 @@ public class PlayerController : MonoBehaviour
   public void FixedUpdate()
   {
      PlayerMovement();
+     PlayerRunAnimation();
   }
 
   private void PlayerMovement()
@@ -28,14 +34,30 @@ public class PlayerController : MonoBehaviour
       Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
       transform.Translate(direction * speed);
 
-      if (speed <= .1)
+      if (joystickBg.gameObject.activeSelf)
       {
-          playerAnimator.SetFloat("Walk",speed);
+          isRunning = true;
+          sword.transform.position = rightHand.position;
       }
-      if (speed <= .3)
+      else
       {
-          playerAnimator.SetFloat("Run",speed);
+          isRunning = false;
+          sword.transform.position = transform.position;
       }
-  }                                                                                     
+  }
+
+  private void PlayerRunAnimation()
+  {
+    
+      if (isRunning)
+      {
+          playerAnimator.SetBool("isRunning", true);
+      }
+      else
+      {
+          playerAnimator.SetBool("isRunning", false);
+      }
+  }
+  
 }
 
